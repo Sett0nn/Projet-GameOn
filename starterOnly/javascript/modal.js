76f2989957b06715d65d0e68b72f2df81ac4ee1a
Sett0nn <pooktommy@hotmail.com>
2024-04-19 03:42:23 +0200

    Work in progress modal.js last work checkbox

--- starterOnly/javascript/modal.js
+++ starterOnly/javascript/modal.js
@@ -3,13 +3,29 @@ const modalBackground = document.querySelector(".bground");
 const modalButtons = document.querySelectorAll(".modal-btn");
 const formInputs = document.querySelectorAll(".formData");
 const closeModalButton = document.querySelector(".close");
+const conditionsAcceptedCheckbox = document.querySelector("#checkbox1");
+const submitButton = document.querySelector(".btn-submit");
 
-// launch modal event
+
 if (modalBackground == null) throw new Error("No modal background found");
+if (conditionsAcceptedCheckbox == null) throw new Error("No checkbox found");
+conditionsAcceptedCheckbox.addEventListener("change", checkIfConditionAccepted);
+  console.log("checkbox change");
 
+modalButtons.forEach((btn) => btn.addEventListener("click", launchModal));
+closeModalButton.addEventListener("click", closeModalButton)
 
-// modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-// closeModalButton.addEventListener("click", closeModalButton)
+function checkIfConditionAccepted(event) {
+  if (submitButton == null) throw new Error("No submit button found");
+  const isChecked = event.target.checked;
+  console.log("isChecked", isChecked);
+  // if (!isChecked) {
+  //   submitButton.setAttribute("disabled", "true");
+  // } else {
+  //   submitButton.setAttribute("disabled")
+  if (isChecked) return submitButton.removeAttribute("disabled");
+  submitButton.setAttribute("disabled", "true")
+}
 
 function closeModal() {
   if (closeModalButton == null ) throw new Error("no close button found")
@@ -20,10 +36,13 @@ function closeModal() {
 function launchModal() {
   if (modalBackground == null) throw new Error("No modal background found")
     modalBackground.classList.add("active");
+  modalBackground.style.display = ("block");
 }
 
 function editNav() {
   const topNavbar = document.getElementById("myTopnav");
-if (TopNavbar == null) throw new Error("No topnav found");
+if (topNavbar == null) throw new Error("No topnav found");
 topNavbar.classList.toggle("responsive");
 }
+
+
